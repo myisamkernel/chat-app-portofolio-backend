ab81b72153fa7a310d26a7e94809972723c6e5a4
myisamkernel <138276597+myisamkernel@users.noreply.github.com>
2025-08-12 16:40:08 +0700

    preparation chat room

--- controllers/chatController.go
+++ controllers/chatController.go
@@ -1,9 +1,19 @@
 package controllers
 
-import "github.com/labstack/echo/v4"
+import (
+	"chat-app-portfolio/config"
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
 
 func CreateNewChatRoom(c echo.Context) error {
-	return c.String(200, "post")
+	ctx := context.Background()
+	redis := config.GetRedisClient()
+	res, _ := redis.Set(ctx, "input_counter", uuid.New(), 0).Result()
+
+	return c.JSON(200, res)
 }
 
 func FetchExistingChatRoom(c echo.Context) error {
